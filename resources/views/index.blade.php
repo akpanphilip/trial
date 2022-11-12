5e01b2b3280634c7b7040790b8d80f3d4d49e309
akpanphilip <akpanphilip1122@gmail.com>
2022-11-12 19:52:22 +0100

    fixed issue on dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.main')
 @section('content')
-<div class="banner">
-    <p class="banner-text">
-        Returning violence for violence multiplies violence, 
-        adding deeper darkness to a night already devoid of stars
-    </p>
-    
-</div>
+    <div class="banner">
+        <p class="banner-text">
+            Returning violence for violence multiplies violence,
+            adding deeper darkness to a night already devoid of stars
+        </p>
 
-<p class="case-text-title text-center"> CASES REPORTED </p>
-<div class="container">
-    <div class="row report-row">
-        @foreach ($reports as $report)
-        <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="cases">
-                <div class="cases-image">
-                    <img src="{{ asset('violenceImage') }}/{{ $report->image }}" alt="">
-                </div>
-                <div class="case-title">
-                    {{ $report->detail }}
-                </div>
-            </div>
-        </div>
-        @endforeach
     </div>
-</div>
+
+    <p class="case-text-title text-center"> CASES REPORTED </p>
+    <div class="container">
+        <div class="row report-row">
+            @foreach ($reports as $report)
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="cases">
+                        <div class="cases-image">
+                            <img src="{{ asset('violenceImage') }}/{{ $report->image }}" alt="">
+                        </div>
+                        <div class="case-title">
+                            {{ $report->detail }}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
